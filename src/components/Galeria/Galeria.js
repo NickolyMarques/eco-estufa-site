fe51909f373c4a8c095ef210eab6b3b1735ed169
Nickoly Marques <roselaine.nickoly@gmail.com>
2018-12-15 18:32:45 +0100

    [feature] add galley functionality

--- src/components/Galeria/Galeria.js
+++ src/components/Galeria/Galeria.js
@@ -1,48 +1,22 @@
 import React, {Component} from 'react';
-import Gallery from 'react-grid-gallery';
+import Lightbox from 'react-images';
 import {recuperaProdutos} from "../../services/recuperaProdutosService";
-
+import Demo4 from "./Demo4";
 
 class Galeria extends Component{
 
-  obtainImages()   {
-      let IMAGES=[];
-  return IMAGES =[{
-        src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
-        thumbnail: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg",
-        thumbnailWidth: 320,
-        thumbnailHeight: 174,
-        isSelected: true,
-        caption: "After Rain (Jeshu John - designerspics.com)"
-},
-{
-        src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
-        thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
-        thumbnailWidth: 320,
-        thumbnailHeight: 212,
-        tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
-        caption: "Boats (Jeshu John - designerspics.com)"
-},
-
-{
-        src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-        thumbnail: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
-        thumbnailWidth: 320,
-        thumbnailHeight: 212
-}];
-  }
-
-
-  
-    
-    render(){
-        
+   render() {
         return(
-           
-            <Gallery images={this.obtainImages()}/>,
-        document.getElementById('example-0')
-            
-            
+            <div className="container" id="quemsomos">
+            <div className="row">
+            <div className="col with-margin-top">
+                       
+                       <Demo4/ >
+                       {document.getElementById('demo4')}
+          </div>
+          </div>
+          </div>
+
         );
     }
 }
